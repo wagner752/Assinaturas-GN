589d9788e1658036dd1016b82a43be1bbf53c5e4
Cleiton Lima <acrl.ribeiro17@gmail.com>
2022-03-25 23:23:52 -0300

    Update main.js

--- _js/main.js
+++ _js/main.js
@@ -7,8 +7,8 @@ assinatura_custom.onload = () => {
     c.height = 257;
     render();
 };
-assinatura_custom.crossOrigin = "anonymous"
-//assinatura_custom.src = "https://github.com/wagner752/Assinaturas-GN/blob/main/aaa.png?raw=true";
+//assinatura_custom.crossOrigin = "anonymous"
+assinatura_custom.src = "https://github.com/wagner752/Assinaturas-GN/_img/assinatura.png?raw=true";
 
 nome.oninput = setor.oninput = render;
 
